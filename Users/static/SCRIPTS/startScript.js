7cd4f660febf4c64ffae4e3b4c2524391757de8b
Santiago Medina López <smedina@iegabo.edu.co>
2025-01-04 13:18:36 -0500

    Commit

---
+++ Users/static/SCRIPTS/startScript.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+const loginbtn = document.getElementById('login');
+const registerbtn = document.getElementById('register');
+
+registerbtn.addEventListener('click', () => {    
+    container.classList.add('active');
+});
+
+loginbtn.addEventListener('click', () => {
+    container.classList.remove('active');
+});
+
