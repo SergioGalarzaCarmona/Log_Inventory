4774b49e8a00cb1e795c98c546e25a759220bff3
SergioGalarzaCarmona <sergio.ss2411@gmail.com>
2025-01-22 21:11:11 -0500

    Update admin registration to use admin.site.register for models in Objects and Users

--- Objects/admin.py
+++ Objects/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Object, ObjectsGroup, TypeTransaction, Transaction
 
 # Register your models here.
-admin.register(Object)
-admin.register(ObjectsGroup)
-admin.register(TypeTransaction)
-admin.register(Transaction)
+admin.site.register(Object)
+admin.site.register(ObjectsGroup)
+admin.site.register(TypeTransaction)
+admin.site.register(Transaction)
