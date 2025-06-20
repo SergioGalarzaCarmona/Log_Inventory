38c96579f8139df4ca6521364d51540c20263c81
Sergio Galarza <sergio.ss2411@gmail.com>
2025-06-20 14:30:44 -0500

    change password subuser

---
+++ Users/static/SCRIPTS/subprofileScript.js
@@ -0,0 +1,18 @@
+const new_password = () => {
+    const checkbox = document.getElementById('change_password')
+    const pivot_password = document.getElementById('pivot_password')
+    const form = document.getElementById('set_password_form')
+    const change_password = document.getElementById('open-dialog')
+    if (checkbox.checked == true){
+        form.style.display = 'block';
+        pivot_password.style.display = 'none';
+        change_password.style.display = 'none';
+    }
+
+    else {
+        form.style.display = 'none';
+        pivot_password.style.display = 'block';
+        change_password.style.display = 'block';
+    }
+        
+}
