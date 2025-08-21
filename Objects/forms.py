0c8e981b5c0cbe3ebd6afed7b8f7941a4ece4be4
Sergio Galarza <sergio.ss2411@gmail.com>
2025-08-20 21:00:10 -0500

    Add CreateObjectForm for object creation with validation and user-specific queryset

---
+++ Objects/forms.py
@@ -0,0 +1,129 @@
+from django import forms
+from .models import Object, ObjectsGroup
+from Users.models import Subprofile
+from django.core.exceptions import ValidationError
+
+class CreateObjectForm(forms.ModelForm):
+    name = forms.CharField(
+        label="Nombre",
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+
+            }
+        ),
+        error_messages={
+            "invalid_lenght" : "El nombre es muy corto o muy largo.",
+            "unique" : "El nombre del objeto solo puede estar resgistrado una sola vez."
+        }
+    )
+    
+    stock = forms.IntegerField(
+        label="Cantidad",
+        required=True,
+        widget=forms.NumberInput(
+            attrs={
+                
+            }
+        ),
+        error_messages={
+            "smaller_than_0" : "La cantidad ingresada del objeto debe ser mayor a 0."
+        }
+    )
+    
+    image = forms.ImageField(
+        label="Imagen",
+        required=False,
+        widget=forms.FileInput(
+            attrs={
+                
+            }
+        )
+    )
+    
+    description = forms.CharField(
+        label="Descripción",
+        required=True,
+        widget=forms.Textarea(
+            attrs={
+                
+            }
+        )
+    )
+    
+    group = forms.ModelChoiceField(
+        queryset=None,
+        label="Grupo",
+        widget=forms.Select(
+            attrs = {
+                
+            }
+        ),
+        error_messages = {
+            "invalid" : "Este groupo no pertenerce al usuario referenciado."
+        }
+    )
+    
+    in_charge = forms.ModelChoiceField(
+        queryset=None,
+        label="Encargado",
+        widget=forms.Select(
+            attrs={
+                
+            }
+        ),
+        error_messages={
+            "invalid" : "Este encargado no pertenece al usuario referenciado."
+        }
+    )
+    
+    class Meta:
+        model=Object
+        fields = ["name", "stock", "image", "description", "group", "in_charge"]
+        
+    def __init__(self, *args,**kwargs):
+        user = kwargs.pop('user',None)
+        self.user = user
+        super().__init__(*args,**kwargs)
+        if user: 
+            self.fields['group'].queryset = ObjectsGroup.objects.filter(user=user)
+            self.fields['in_charge'].queryset = Subprofile.objects.filter(user=user)
+        
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if len(name) < 3 or len(name) > 100:
+            error = ValidationError(self.fields['name'].error_messages['invalid_lenght'],
+                                    code="invalid_lenght")
+            self.add_error('name',error)
+        if Object.objects.filter(name=name, user=self.user):
+            error = ValidationError(self.fields['name'].error_messages['unique'],
+                                    code="unique")
+            self.add_error('name', error)
+        return name 
+    
+    def clean_stock(self):
+        stock = self.cleaned_data.get('stock')
+        stock = int(stock)
+        if stock < 1:
+            error = ValidationError(self.fields['stock'].error_messages['smaller_than_0'],
+                                    code="smaller_than_0")
+            self.add_error('stock', error)
+        return stock 
+    
+    def clean_group(self):
+        group = self.cleaned_data.get('group')
+        if group.user != self.user:
+            error = ValidationError(self.fields['group'].error_messages['invalid'],
+                                    code="invalid")
+            
+            self.add_error('group', error)
+        return group
+    
+    def clean_in_charge(self):
+        in_charge = self.cleaned_data.get('in_charge')
+        if in_charge.profile.user != self.user:
+            error = ValidationError(self.fields['in_charge'].error_messages['invalid'],
+                                    code="invalid")
+            self.add_error('in_charge', error)
+        return in_charge
+    
