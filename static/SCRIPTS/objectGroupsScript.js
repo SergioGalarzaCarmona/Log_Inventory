14d8e6c32834d769c7b9215edca8b5f10ab4db48
Santiago Medina López <smedina@iegabo.edu.co>
2025-09-24 21:05:05 -0500

    feat: Add styles for object groups and enhance sidebar interactions

--- static/SCRIPTS/objectGroupsScript.js
+++ static/SCRIPTS/objectGroupsScript.js
@@ -93,3 +93,11 @@ document.addEventListener("DOMContentLoaded", () => {
     
     toggleTableVisibility();
 });
+
+document.querySelectorAll(".group-header").forEach(header => {
+  header.addEventListener("click", () => {
+    const container = header.parentElement;
+    container.classList.toggle("open");
+    header.classList.toggle("active");
+  });
+});
