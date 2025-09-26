183961e308c72c25d66c52e3cf196d48add14c44
Sergio Galarza <sergio.ss2411@gmail.com>
2025-09-26 12:09:31 -0500

    feat: Add Borrowings and LiveChat apps with initial models and migrations; remove obsolete Borrowings model from Objects

--- LogInventory/settings.py
+++ LogInventory/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Users.apps.UsersConfig',
     'Objects',
+    'Borrowings',
+    'LiveChat',
 ]
 
 MIDDLEWARE = [
--- Objects/models.py
+++ Objects/models.py
@@ -145,26 +145,3 @@ class Transaction(models.Model):
     class Meta:
         verbose_name = 'Transaction'
         verbose_name_plural = 'Transactions'
-
-class Borrowings(models.Model):
-    object = models.ForeignKey(
-        Objects,
-        on_delete=models.CASCADE
-    )
-    in_charge = models.ForeignKey(
-        Subprofile,
-        on_delete=models.CASCADE
-    )
-    date_init = models.DateTimeField(
-        auto_now_add=True
-    )
-    date_limit = models.DateTimeField()
-    date_complete = models.DateTimeField()
-    stock = models.IntegerField()
-    
-    def __str__(self):
-        return f'{self.object.name} Borrowing'
-    
-    class Meta:
-        verbose_name = 'Borrowing'
-        verbose_name_plural = 'Borrowings'
