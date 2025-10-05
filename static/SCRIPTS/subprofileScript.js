d515cfc0754be42b1bec9b043ff28adb44baf130
Sergio Galarza <sergio.ss2411@gmail.com>
2025-10-05 16:41:00 -0500

    feat: Add delete button functionality for profile images and enhance alert message handling

--- static/SCRIPTS/subprofileScript.js
+++ static/SCRIPTS/subprofileScript.js
@@ -35,6 +35,14 @@ profile_image_input.addEventListener("change", function () {
   document.getElementById("id_submit").click();
 });
 
+// This script handles the delete button functionality for profile images.
+const delete_button = document.getElementById("id_delete_button");
+delete_button.addEventListener("click", function () {
+  const checkbox = document.getElementById("id_delete_checkbox");
+  checkbox.checked = true;
+  document.getElementById("delete_image").click();
+});
+
 // This script handles the delete button functionality for subprofile images.
 document.addEventListener("DOMContentLoaded", () => {
   const deleteBtn = document.getElementById("delete-button");
@@ -61,6 +69,22 @@ document.addEventListener("DOMContentLoaded", () => {
   modal.addEventListener("click", (e) => {
     if (e.target === modal) {
       modal.style.display = "none";
+    }
+  });
+});
+
+
+
+// Handle all close button to alert messages
+const closeButtons = document.querySelectorAll(".message-close-button");
+
+closeButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const alert = button.closest(
+      ".message-error, .message-success, .message-warning"
+    );
+    if (alert) {
+      alert.classList.add("hidden");
     }
   });
 });
