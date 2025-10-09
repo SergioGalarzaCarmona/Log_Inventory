3ae71f78a63c4c69ab41b51e5afa6c6e8aefd652
Sergio Galarza <sergio.ss2411@gmail.com>
2025-10-09 16:16:26 -0500

    no static files

--- LogInventory/settings.py
+++ LogInventory/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0!5sff-zy51at=)uvkz2iwi=lsna4b93vn-c=s$9n5vluccb=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*", "localhost", "web", "0.0.0.0", "localhost"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
