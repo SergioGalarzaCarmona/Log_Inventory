559e0743c0e2cd9a74f9d51196d6fe7bbd4ec6f1
Sergio Galarza <sergio.ss2411@gmail.com>
2025-10-09 13:50:02 -0500

    Hello world, I'm alive

--- LogInventory/settings.py
+++ LogInventory/settings.py
@@ -148,7 +148,8 @@ LOGIN_URL = '/authenticate_user/deactive'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
