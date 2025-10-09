0afb48e0f9cd9ec9aa54398884a87f6bfd3d939d
Sergio Galarza <sergio.ss2411@gmail.com>
2025-10-09 16:46:49 -0500

    update db

--- LogInventory/settings.py
+++ LogInventory/settings.py
@@ -99,11 +99,11 @@ DATABASES = {
     "default": 
         {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT", 5432),
+        "NAME": "log_inventory",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
         }
 }
 
