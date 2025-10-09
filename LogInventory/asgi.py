600ab93f28bf3661afd5e1b26057ce13edb422aa
Sergio Galarza <sergio.ss2411@gmail.com>
2025-10-09 13:35:45 -0500

    upload imports in asgi.py to fix log error in docker compose

--- LogInventory/asgi.py
+++ LogInventory/asgi.py
@@ -2,13 +2,17 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-from LiveChats import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yourproject.settings")
 
+django_asgi_app = get_asgi_application()
+
+from LiveChats import routing
+
+
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
     }
 )
