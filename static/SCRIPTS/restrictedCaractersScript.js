c851a09f329aeb9f0382751bf0e89d0378b4673b
Sergio Galarza <sergio.ss2411@gmail.com>
2025-10-11 11:52:28 -0500

    fixing erros with restricted values in some inputs type text

--- static/SCRIPTS/restrictedCaractersScript.js
+++ static/SCRIPTS/restrictedCaractersScript.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     input.addEventListener("input", e => {
       const pattern = "/[a-zA-Z]/" 
       const expression = new RegExp(pattern)
+      console.log(e.target.value)
       if (!expression.test(e.target.value)) {
+        console.log(e.target.value)
         e.preventDefault()
       }
     })
