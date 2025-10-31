179888296632ec57fda8b29a1063d54b50fb3377
Sergio Galarza <sergio.ss2411@gmail.com>
2025-10-30 23:39:14 -0500

    Update email settings to use Hostinger SMTP server

--- LogInventory/settings.py
+++ LogInventory/settings.py
@@ -172,11 +172,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email settings
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.hostinger.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'log.inventory2406@gmail.com'
-EMAIL_HOST_PASSWORD = 'bqvp xrku umwg siov'
+EMAIL_HOST_USER = 'soporte@log-inventory.com'
+EMAIL_HOST_PASSWORD = 'mazamorra'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
   
